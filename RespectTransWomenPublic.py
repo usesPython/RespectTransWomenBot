301cb7cdb2914e9c719dcbfa694db709b5d9fc83
usesPython <54753780+usesPython@users.noreply.github.com>
2019-09-14 18:02:18 +0200

    Improved handling of critical sections
    Shutdowns are now faster as the can happen at any time except for the case in which a comment is being replied to in which case it will wait until the reply id is stored (unless another shutdown signal is sent in which case it will force quit and might not save the reply id)
    
    Make sure to shutdown by raising SIGINT (Normally done by pressing ctrl-c)

--- RespectTransWomenPublic.py
+++ RespectTransWomenPublic.py
@@ -1,44 +1,48 @@
 import praw #Used to interact with the reddit API
 import signal #Used to know when we need to exit
-from sys import exit #Preferred over default exit() (See: https://stackoverflow.com/questions/6501121/difference-between-exit-and-sys-exit-in-python/6501134#6501134
+from sys import exit #Preferred over default exit() (See: https://stackoverflow.com/questions/6501121/difference-between-exit-and-sys-exit-in-python/6501134#6501134 )
 
-class GracefulExiter():
-    #Handles SIGINT (Signal interrupt) signals (Raised by pressing Ctrl-C)
-    #Instantiate it using with since that allows us to set the SIGINT handler back to SIG_DFL when we're out of a critical section
+class CriticalSectionHandler():
+    #Prevents python from shutting down while CriticalSectionHandler() exists
     #Example usage:
-    #with GracefulExiter() as exiter:
-    #   while True:
-    #       #do stuff
-    #       if exiter.get_exit_flag()
-    #           #handle exiting gracefully
-    #Prefered over:
-    #from threading import Thread
-    #try:
-    #   thread = Thread(target=func, args=(foo,bar))
-    #   thread.start()
-    #   thread.join()
-    #catch KeyboardInterrupt:
+    #with CriticalSectionHandler() as csh:
     #   #do stuff
-    #In cases where code in the catch block being run while thread is being run causes errors (can happen if KeyboardInterrupt is raised between thread.start() and thread.join())
-        
-    def __init__(self):
-        self.state = False #Has SIGINT been raised?
-        signal.signal(signal.SIGINT, self.change_state) #If SIGINT is raised then run self.change_state()
+    #or:
+    #csh = CriticalSectionHandler()
+    #csh.enter_critical_section()
+    ##do stuff
+    #csh.exit_critical_section()
         
     def __enter__(self):
-        self.state = False
+        self.state = False #True if SIGINT has been raised, False otherwise
+        self.signal = signal.getsignal(signal.SIGINT) #Save the previous SIGINT handler
+        signal.signal(signal.SIGINT, self.change_state) #If SIGINT is raised then run self.change_state()
+        return self
+    
+    def __exit__(self, exception_type, exception_value, traceback):
+        signal.signal(signal.SIGINT, self.signal) #If SIGINT is raised after this point then run the previous SIGINT handler (Generally SIG_DEF which usually shuts down and core dumps)
+        if self.state: #Final check to see if SIGINT was raised during the critical section
+            self.signal()
+    
+    def __del__(self):
+        signal.signal(signal.SIGINT, self.signal) #If SIGINT is raised after this point then run the previous SIGINT handler (Generally SIG_DEF which usually shuts down and core dumps)
+        if self.state: #Final check to see if SIGINT was raised during the critical section
+            self.signal()
+    
+    def enter_critical_section(self):
+        self.state = False #Has SIGINT been raised?
+        self.signal = signal.getsignal(signal.SIGINT) #Save the previous SIGINT handler
         signal.signal(signal.SIGINT, self.change_state) #If SIGINT is raised then run self.change_state()
     
-    def __exit__(self):
-        signal.signal(signal.SIGINT, signal.SIG_DFL) #If SIGINT is raised after this point then run the default SIGINT handler (Generally kills the program, but technically is implementation defined)
-    
+    def exit_critical_section(self):
+        signal.signal(signal.SIGINT, self.signal) #If SIGINT is raised after this point then run the previous SIGINT handler (Generally SIG_DEF which usually shuts down and core dumps)
+        if self.state: #Final check to see if SIGINT was raised during the critical section
+            self.signal()
+
     def change_state(self, signum, frame):
-        print("Exiting, repeat to shutdown immediately.")
+        print("Shutting down...")
         self.state = True #SIGINT has been raised
-        signal.signal(signal.SIGINT, signal.SIG_DFL) #If SIGINT is raised after this point then run the default SIGINT handler (Generally kills the program, but technically is implementation defined)
-    
-    def get_exit_flag(self): #Returns True if SIGINT has been raised after object has been created, False otherwise
-        return self.state
+        signal.signal(signal.SIGINT, self.signal) #If SIGINT is raised after this point then run the previous SIGINT handler (Generally SIG_DEF which usually shuts down and core dumps)
 
 def main():
     #Initialize the bot
@@ -157,49 +161,46 @@ def main():
     
     print("Bot initialized, starting main loop")
 
-    with GracefulExiter() as exiter: #Used to check if we need to exit
-        for comment in comments: #Current comment being processed is an object called comment . See https://praw.readthedocs.io/en/latest/code_overview/models/comment.html for more details on the comment object
-            if isValidComment(comment, blocked_subreddits, blocked_users, reply_ids, trigger_words): #Preliminary check to see if we might be allowed to post on this comment
-                #Further checking to verify we actually caught a slur
-                formatted_comment = formatComment(comment)
-                caught_words = slur_check(formatted_comment, trigger_words)
-                if caught_words:
-                    #Valid slur detected
-                    message = formatReply(caught_words)
-                    if reddit.read_only == False:
-                        try:
+    for comment in comments: #Current comment being processed is an object called comment . See https://praw.readthedocs.io/en/latest/code_overview/models/comment.html for more details on the comment object
+        if isValidComment(comment, blocked_subreddits, blocked_users, reply_ids, trigger_words): #Preliminary check to see if we might be allowed to post on this comment
+            #Further checking to verify we actually caught a slur
+            formatted_comment = formatComment(comment)
+            caught_words = slur_check(formatted_comment, trigger_words)
+            if caught_words:
+                #Valid slur detected
+                message = formatReply(caught_words)
+                if reddit.read_only == False:
+                    try:
+                        with CriticalSectionHandler() as csh: #Stops the program from stopping until after this section is done since not storing the comment id means we might reply to the same comment multiple times
                             comment.reply(message)
                             reply_ids.append(str(comment.id).lower())
-                        except praw.exceptions.Forbidden: #Cannot post comment
-                            print("Failed to post comment. Check if you are allowed to comment in the subreddit and if your login credentials are correct.")
-                            print("Subreddit: /r/" + str(comment.subreddit)) #DEBUG INFORMATION
-                            #raise #If problem persists uncomment raise so you can check the full error
-                    else: #DEBUG MODE
-                        print("Subreddit: /r/" + str(comment.subreddit))
-                        print("Comment id:", str(comment.id))
-                        try:
-                            print("Author:", str(comment.author))
-                        except AttributeError:
-                            print("Author Deleted")
-                        print("Original comment:")
-                        print(str(comment.body))
-                        print("Formatted comment:")
-                        print(formatted_comment)
-                        print("Caught words:")
-                        print(caught_words)
-                        print("Message reply:")
-                        print(message)
-            if exiter.get_exit_flag(): #SIGINT has been raised so we need to exit gracefully ASAP
-                #Append new reply_ids to replyids.txt
-                try:
-                    with open("D:/Reddit Bots/Respect Trans Women/replyids.txt", "a") as fs: #Path to replyids.txt
-                        while new_reply_index < len(reply_ids):
-                            fs.write('\n' + reply_ids[new_reply_index]) #Preferred over reply_ids[new_reply_index] + '\n' since this guarantees all new ids will not be corrupted if writing is not interrupted
-                            new_reply_index += 1
-                    exit(0) #Reply ids are stored so we can send exit code zero, signalling "successful termination"
-                except Exception:
-                    print("Failed to store new reply ids to replyids.txt!")
-                    exit(1) #Since we couldn't store replys we send a non-zero exit code, signalling "abnormal termination"
+                            #Append new reply_ids to replyids.txt
+                            try:
+                                with open("D:/Reddit Bots/Respect Trans Women/replyids.txt", "a") as fs: #Path to replyids.txt
+                                    while new_reply_index < len(reply_ids): #We use a while loop so that if we fail to write to the file we don't lose the id and can try again with the next comment
+                                        fs.write('\n' + reply_ids[new_reply_index]) #Preferred over reply_ids[new_reply_index] + '\n' since this guarantees all new ids will not be corrupted if writing is not interrupted
+                                        new_reply_index += 1
+                            except Exception:
+                                print("Failed to store new reply ids to replyids.txt!")
+                    except praw.exceptions.Forbidden: #Cannot post comment
+                        print("Failed to post comment. Check if you are allowed to comment in the subreddit and if your login credentials are correct.")
+                        print("Subreddit: /r/" + str(comment.subreddit)) #DEBUG INFORMATION
+                        #raise #If problem persists uncomment raise so you can check the full error
+                else: #DEBUG MODE
+                    print("Subreddit: /r/" + str(comment.subreddit))
+                    print("Comment id:", str(comment.id))
+                    try:
+                        print("Author:", str(comment.author))
+                    except AttributeError:
+                        print("Author Deleted")
+                    print("Original comment:")
+                    print(str(comment.body))
+                    print("Formatted comment:")
+                    print(formatted_comment)
+                    print("Caught words:")
+                    print(caught_words)
+                    print("Message reply:")
+                    print(message)
 
 def isValidComment(comment, blocked_subreddits, blocked_users, reply_ids, trigger_words):
     #Fully checks if the comment is valid based on everything but the body of the comment and partially checks if the comment might be valid based on the body of the comment
